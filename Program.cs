7ef2abcf7df45314d76735fec166703ec1b20f27
fitosjozsef0309 <fitos.jozsef.2024d@telekimezotur.hu>
2026-09-16 12:09:54 +0200

    2.feladat.32,feladat.22.feladat.1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("");
+﻿int ar = 0;
+
+Console.WriteLine("Kérem a vásárló nevét: ");
+
+string vnev = Console.ReadLine();
+
+
+Console.WriteLine("Kérem a vásárló életkorát(év)");
+int Vkor = int.Parse(Console.ReadLine());
+
+var van = true;
+
+Console.WriteLine("Van Diák igazolvány?(1=igen)");
+var igazolv = int.Parse(Console.ReadLine());
+var kedvezmeny = true;
+if (igazolv == 1)
+{
+    van = true;
+}
+else
+{
+    van = false;
+}
+if van == true{
+    Console.WriteLine("A vásárló diák, kedvezményes jegyárat kap.");
+
+};
+
+
+
+Console.WriteLine("A jegy ára:");
+decimal Jegyar = decimal.Parse(Console.ReadLine());
+
+int Popcornar = 1500;
+Console.WriteLine("Popcorn mennyisége:");
+decimal Popcar = decimal.Parse(Console.ReadLine());
+
+Popcornar = int.Parse("1500")*Popcar;
+
+int osszeg1 = (int)Jegyar + Popcornar;
+
+if (igazolv == 1)
+{
+    int osszeg = (int)Jegyar + Popcornar+((int)Jegyar + Popcornar/100*20);
+    Console.WriteLine($"Az összeg: {osszeg}");
+}
+;
